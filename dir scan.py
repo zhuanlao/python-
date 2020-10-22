b22d2ac68d0610fddbfcbbba6931cb0b5b076a66
zhuanli <42299567+zhuanlao@users.noreply.github.com>
2020-10-22 18:17:14 +0800

    Update dir scan.py

--- dir scan.py
+++ dir scan.py
@@ -1,8 +1,8 @@
 import requests
 import sys
 
-url = sys.argv[1]
-dir = sys.argv[2]
+url = sys.argv[1] //url参数
+dir = sys.argv[2] //字典参数
 with open("dir","r") as f:
     for line in f.readlines():
         line = line.strip()
