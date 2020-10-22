361d0c07d4d7cfd0d514ce4a312b58ca5e71de65
zhuanli <42299567+zhuanlao@users.noreply.github.com>
2020-10-22 18:20:16 +0800

    Update dir scan.py

--- dir scan.py
+++ dir scan.py
@@ -1,11 +1,14 @@
 import requests
 import sys
 
-url = sys.argv[1] //url参数
-dir = sys.argv[2] //字典参数
+url = sys.argv[1] #url参数
+dir = sys.argv[2] #字典参数
+#打开字典拼接url
 with open("dir","r") as f:
     for line in f.readlines():
         line = line.strip()
+        #http get请求url
         r = requests.get(url+line)
+        #判断状态码，输出存在目录
         if r.status_code == 200:
             print("url:"+r.url+"exist")
